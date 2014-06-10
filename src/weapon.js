48acc880fbab54c22ba75138d14be74cd77535fc
xy124 <qcd@gmx.net>
2014-06-10 18:20:05 +0200

    walls stop projectiles

--- src/weapon.js
+++ src/weapon.js
@@ -30,7 +30,7 @@ Projectile = Class.create(Sprite, {
 			var s = trajectory(that.x, that.y,  that.age);
 			that.moveTo(s.x, s.y);
 
-			if ( that.age > 5000 || that.x > game.width || that.x < 0 - that.width || that.y > game.height || that.y < 0 - that.height) {
+			if ( that.age > 5000 || that.x > game.width || that.x < 0 - that.width || that.y > game.height || that.y < 0 - that.height || !game.map.hitTest(s.x + that.width / 2, s.y + that.height / 2)) { // weapons can't transmit walls.
 				die();
 			}
 
